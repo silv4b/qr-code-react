ab18db93125e7ae8984df02dcba3be2a3512fe3d
Bruno Silva <brunosilva@viggosistemas.com.br>
2023-05-27 00:34:24 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
+import { useState } from 'react';
+import Base64Downloader from 'react-base64-downloader';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [qrCodeValue, setQrCodeValue] = useState('');
+  const [linkQrCode, setQrCodeLink] = useState('');
+
+  function handleQrCode(evt) {
+    setQrCodeValue(evt.target.value);
+    handleGenerate(qrCodeValue);
+  }
+
+  function handleGenerate(linkUrl) {
+    QRCodeLink.toDataURL(
+      linkUrl, {
+      width: 600,
+      margin: 3
+    }, function (err, url) {
+      setQrCodeLink(url);
+    }
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container'>
+      <QRCode
+        value={qrCodeValue}
+        size={316}
+      />
+
+      <input
+        className='input'
+        placeholder='Digite seu texto aqui ...'
+        value={qrCodeValue}
+        onChange={(evt) => handleQrCode(evt)}
+      />
+
+      <Base64Downloader
+        className='buttom'
+        base64={linkQrCode}
+        downloadName="qrcode"
+        onDownloadSuccess={() => console.log('File download initiated')}
+        onDownloadError={() => console.warn('Download failed to start')}
+      >
+        Salvar QRCode
+      </Base64Downloader>
+    </div >
   );
 }
 
-export default App;
+// export default App;
