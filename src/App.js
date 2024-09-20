ea3d61bb2d933ba5c8d077e4459831ed579d5bcb
silv4b <brunno.linkin@gmail.com>
2024-09-19 22:09:31 -0300

    corrigindo botão de download do qrcode gerado e o mesmo só aparece quandoa algo é digitado no campo específico

--- src/App.js
+++ src/App.js
@@ -1,54 +1,57 @@
-import QRCode from 'react-qr-code';
-import QRCodeLink from 'qrcode';
-import { useState } from 'react';
-import Base64Downloader from 'react-base64-downloader';
-import './App.css';
+import QRCode from "react-qr-code";
+import QRCodeLink from "qrcode";
+import { useState } from "react";
+import Base64Downloader from "react-base64-downloader";
+import "./App.css";
 
 export default function App() {
-  const [qrCodeValue, setQrCodeValue] = useState('');
-  const [linkQrCode, setQrCodeLink] = useState('');
+    const [qrCodeValue, setQrCodeValue] = useState("");
+    const [linkQrCode, setQrCodeLink] = useState("");
+    const [hideDownloadButton, setHideDownloadButton] = useState(true);
 
-  function handleQrCode(evt) {
-    setQrCodeValue(evt.target.value);
-    handleGenerate(qrCodeValue);
-  }
+    function handleQrCode(evt) {
+        const qr_code_aux = evt.target.value.trim();
+        setQrCodeValue(qr_code_aux);
 
-  function handleGenerate(linkUrl) {
-    QRCodeLink.toDataURL(
-      linkUrl, {
-      width: 600,
-      margin: 3
-    }, function (err, url) {
-      setQrCodeLink(url);
+        if (qr_code_aux === "") {
+            setHideDownloadButton(false);
+        } else {
+            setHideDownloadButton(true);
+        }
+        handleGenerate(qrCodeValue);
     }
+
+    function handleGenerate(linkUrl) {
+        QRCodeLink.toDataURL(
+            linkUrl,
+            {
+                width: 600,
+                margin: 3,
+            },
+            function (err, url) {
+                setQrCodeLink(url);
+            }
+        );
+    }
+
+    return (
+        <div className="container">
+            <QRCode value={qrCodeValue} size={316} />
+            <input
+                className="input"
+                placeholder="Digite seu texto ou link aqui ..."
+                value={qrCodeValue}
+                onChange={(evt) => handleQrCode(evt)}
+            />
+            {hideDownloadButton && (
+                <Base64Downloader
+                    base64={linkQrCode}
+                    downloadName="my_qr_code_Base64Downloader"
+                >
+                    Salvar QR Code
+                </Base64Downloader>
+            )}
+        </div>
     );
-  }
-
-  return (
-    <div className='container'>
-      <QRCode
-        value={qrCodeValue}
-        size={316}
-      />
-
-      <input
-        className='input'
-        placeholder='Digite seu texto aqui ...'
-        value={qrCodeValue}
-        onChange={(evt) => handleQrCode(evt)}
-      />
-
-      <Base64Downloader
-        className='buttom'
-        base64={linkQrCode}
-        downloadName="qrcode"
-        onDownloadSuccess={() => console.log('File download initiated')}
-        onDownloadError={() => console.warn('Download failed to start')}
-      >
-        Salvar QRCode
-      </Base64Downloader>
-    </div >
-  );
 }
 
-// export default App;
